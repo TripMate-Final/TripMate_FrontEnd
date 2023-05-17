b4dfa9887215d58ee213f7b529f53c402cd5caea
yerimhuh <yerimhuh@naver.com>
2023-05-18 01:16:31 +0900

    :rocket: feat: 네비게이션바 구현

--- src/components/TheHeaderNavbar.vue
+++ src/components/TheHeaderNavbar.vue
@@ -1,21 +1,26 @@
 <template>
-    <div>
-        <b-navbar b-row variant="light">
-            <b-col>
-                <b-navbar-brand href="#">
-                    <img src="../assets/img/navbar/logo.png" style="float:right">
-                </b-navbar-brand>
-            </b-col>
-            <b-col cols="8">
-                <b-form-input placeholder="검색어를 입력하세요"></b-form-input>
-            </b-col>
-           <b-col>
-               <b-navbar-brand>
-                   <img src="../assets/img/navbar/map.png" style="height:50px; float:left">
-               </b-navbar-brand>
-           </b-col>
-        </b-navbar>
-    </div>
+  <div>
+    <b-container class="bv-example-row text-center">
+      <b-navbar toggleable >
+        <b-col cols="2">
+          <b-navbar-brand href="#" >
+            <img src="../assets/img/navbar/logo.png"/>
+          </b-navbar-brand>
+        </b-col>
+        <b-col cols="9">
+          <b-form class="d-flex">
+            <b-form-input placeholder="Search" ></b-form-input>
+            <b-button id="submitBtn" type="submit">Search</b-button>
+          </b-form>
+        </b-col>
+        <b-col cols="1">
+          <b-navbar-brand id="map" href="#" >
+            <img src="../assets/img/navbar/map.png"/>
+          </b-navbar-brand>
+        </b-col>
+      </b-navbar>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -25,11 +30,16 @@
 </script>
 
 <style scoped>
-.navbar-brand img {
-    height: 70px;
+img{
+  height:50px;
+  padding:0px;
 }
-.form-control{
-    background-color: #62B273;
-    opacity: 0.3;
+#map{
+  margin-bottom: 10px;
+}
+#submitBtn{
+  background-color: #62B273;
+  opacity: 0.8;
+  ;
 }
 </style>
