24ec624e15acb1e01e494d64cd3c7dd8976b7150
yerimstar <yerimhuh@naver.com>
2023-05-19 17:50:18 +0900

    :rocket: feat: 네비게이션 바 수정

--- src/components/TheHeaderNavbar.vue
+++ src/components/TheHeaderNavbar.vue
@@ -1,26 +1,60 @@
 <template>
-  <div>
-    <b-container class="bv-example-row text-center">
-      <b-navbar toggleable >
-        <b-col cols="2">
-          <b-navbar-brand href="#" >
-            <img src="../assets/img/navbar/logo.png"/>
-          </b-navbar-brand>
-        </b-col>
-        <b-col cols="9">
-          <b-form class="d-flex">
-            <b-form-input placeholder="Search" ></b-form-input>
-            <b-button id="submitBtn" type="submit">Search</b-button>
-          </b-form>
-        </b-col>
-        <b-col cols="1">
-          <b-navbar-brand id="map" href="#" >
-            <img src="../assets/img/navbar/map.png"/>
-          </b-navbar-brand>
-        </b-col>
-      </b-navbar>
-    </b-container>
-  </div>
+    <div>
+        <b-navbar toggleable="lg" type="light" >
+            <b-navbar-brand href="#">
+                <img class="logo" src="../assets/img/navbar/logo.png">
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="#">홈</b-nav-item>
+                    <b-nav-item href="#">여행정보</b-nav-item>
+                    <b-nav-item href="#">여행지도</b-nav-item>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form>
+
+                    <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                        <template #button-content>
+                            <em>User</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+
+    <!--    <b-container class="bv-example-row text-center">-->
+<!--      <b-navbar toggleable >-->
+<!--        <b-col cols="2">-->
+<!--          <b-navbar-brand href="#" >-->
+<!--            <img src="../assets/img/navbar/logo.png"/>-->
+<!--          </b-navbar-brand>-->
+<!--        </b-col>-->
+<!--        <b-col cols="9">-->
+<!--          <b-form class="d-flex">-->
+<!--            <b-form-input placeholder="Search" ></b-form-input>-->
+<!--            <b-button id="submitBtn" type="submit">Search</b-button>-->
+<!--          </b-form>-->
+<!--        </b-col>-->
+<!--        <b-col cols="1">-->
+<!--          <b-navbar-brand id="map" href="#" >-->
+<!--            <img src="../assets/img/navbar/map.png"/>-->
+<!--          </b-navbar-brand>-->
+<!--        </b-col>-->
+<!--      </b-navbar>-->
+<!--    </b-container>-->
+
 </template>
 
 <script>
@@ -30,16 +64,7 @@
 </script>
 
 <style scoped>
-img{
-  height:50px;
-  padding:0px;
-}
-#map{
-  margin-bottom: 10px;
-}
-#submitBtn{
-  background-color: #62B273;
-  opacity: 0.8;
-  ;
+.logo{
+    height: 80px;
 }
 </style>
