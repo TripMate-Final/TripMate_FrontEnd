6edb0b181a06eebc510214c16e45717f0c7f3bb4
yerimstar <yerimhuh@naver.com>
2023-05-19 14:47:56 +0900

    :rocket: feat: 지도 사이드바 목록

--- src/components/map/SideBarList.vue
+++ src/components/map/SideBarList.vue
@@ -2,24 +2,39 @@
   <div class="side-bar-wrapper">
       <div class="side-bar">
           <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
-          <b-sidebar id="sidebar-1" title="Sidebar" shadow>
-              <div class="px-3 py-2 sidebar-text">
-                  <p>
-                      Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-                      in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                  </p>
-                  <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+          <b-sidebar id="sidebar-1" shadow>
+              <div class="sidebar-top">
+                  <side-bar-list-top></side-bar-list-top>
+                  <side-bar-list-tab></side-bar-list-tab>
+              </div>
+              <div class="sidebar-list">
+                  <attraction-card
+                      onclick=""
+                      v-for="tag in attractionList"
+                      :key="tag"
+                      :title="tag"
+                      remove="false"
+                  >
+                  </attraction-card>
               </div>
           </b-sidebar>
       </div>
   </div>
 </template>
 <script>
+import SideBarListTop from "@/components/map/SideBarListTop.vue";
+import SideBarListTab from "@/components/map/SideBarListTab.vue";
+import AttractionCard from "@/components/attraction/item/AttractionCard.vue";
 export default {
     name:'SideBarList',
+    components:{
+        SideBarListTop,
+        SideBarListTab,
+        AttractionCard
+    },
     data(){
         return {
-
+            attractionList: ["11", "11", "11", "11", 1, 1, 1, 1, 1, 1, 1],
         }
     },
     methods:{
@@ -29,11 +44,17 @@ export default {
 
 </script>
 <style lang="scss" scoped>
-
 ::v-deep{
     .b-sidebar{
-        background-color: #62B273 !important;
+        background-color: #fff !important;
         height: 100%;
     }
+    .sidebar-list{
+        background-color: #62B273;
+    }
+}
+.sidebar-top{
+  padding: 10px 15px;
+  position: relative;
 }
 </style>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -25,7 +25,7 @@ export default {
 
 .sidebar-list{
     position:absolute;
-    width:300px;
+    width:600px;
     height:100%;
     //background-color:#62B273;
     z-index:99
