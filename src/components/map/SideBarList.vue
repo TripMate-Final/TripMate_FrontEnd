b0772a12cd599b7d041255c585d67218934969a6
yerimhuh <yerimhuh@naver.com>
2023-05-22 21:24:19 +0900

    :bug: fix: 목록보기 버튼 디자인 수정

--- src/components/map/SideBarList.vue
+++ src/components/map/SideBarList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-button v-b-toggle.sidebar-list>목록보기</b-button>
+        <b-button id="show-list-btn" v-b-toggle.sidebar-list>목록보기</b-button>
         <b-sidebar width="400px" id="sidebar-list"  shadow>
             <div class="px-3 py-2">
                 <side-bar-list-top></side-bar-list-top>
@@ -26,6 +26,30 @@ export default {
 </script>
 <style scoped lang="scss">
 ::v-deep {
+    #show-list-btn{
+      float: right;
+      height: 35px;
+      z-index: 3;
+      margin: 20px;
+      padding: 0px 10px;
+      background-color: #EFECEC;
+      border: 1px solid #0d172a;
+      border-radius: 1.5rem;
+      color: #0d172a;
+      font-family: "NanumSquareOTF", "RocGrotesk-Wide";
+      font-size: 15px;
+      font-weight: bold;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      text-align: left;
+    }
+    #show-list-btn:hover{
+        background-color: #569A64;
+        color: #fff;
+        border: 0px;
+    }
     #sidebar-list{
         .b-sidebar-body{
             overflow-y: hidden;
