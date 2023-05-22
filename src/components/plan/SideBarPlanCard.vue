6ab973efbdaea4748df27ad1f51ee74a7d5ee07e
yerimhuh <yerimhuh@naver.com>
2023-05-23 01:13:58 +0900

    :rocket: feat: 계획 리스트

---
+++ src/components/plan/SideBarPlanCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <b-card header-tag="header">
+        <template #header>
+          <h6 class="mb-0">{{title}}</h6>
+        </template>
+        <b-card-text>{{content}}</b-card-text>
+      </b-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "SideBarPlanTop",
+  props: {
+    title: String,
+    content: String,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/components/plan/SideBarPlanList.vue
+++ src/components/plan/SideBarPlanList.vue
@@ -2,21 +2,45 @@
   <div>
     <b-sidebar id="sidebar-plan" width="400px" right shadow>
       <side-bar-plan-top></side-bar-plan-top>
+      <side-bar-plan-card v-for="item in dummyData"
+                          :key="item.id"
+                          :title="item.title" :content="item.content"
+                          remove="false">
+
+      </side-bar-plan-card>
     </b-sidebar>
   </div>
 </template>
 <script>
 
 import SideBarPlanTop from "@/components/plan/SideBarPlanTop.vue";
-
+import SideBarPlanCard from "@/components/plan/SideBarPlanCard.vue";
 export default {
   name: 'SideBarPlanList',
   components: {
-    SideBarPlanTop
+    SideBarPlanTop,
+    SideBarPlanCard
   },
   data() {
     return {
-      title: '',
+      dummyData: [
+        {
+          id: 1,
+          title: '1일차',
+          content: '1일차입니다..',
+        },
+        {
+          id: 2,
+          title: '2일차',
+          content: '2일차입니당',
+        },
+        {
+          id: 3,
+          title: '3일차',
+          content: '3일차입니당',
+        },
+        // 더 많은 더미 데이터를 추가할 수 있습니다.
+      ],
     }
   },
 }
