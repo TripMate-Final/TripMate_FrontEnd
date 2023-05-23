9a476b309a6474f6463449dd0d921a3f844f2a55
yerimstar <yerimhuh@naver.com>
2023-05-23 15:30:18 +0900

    :rocket: feat: 지도 관광지 목록

--- src/components/map/SideBarAttractionList.vue
+++ src/components/map/SideBarAttractionList.vue
@@ -1,31 +1,37 @@
 <template>
   <div class="sidebar-attraction-list">
     <attraction-card
-        v-for="tag in attractionList"
-        :key="tag"
-        :title="tag"
-        remove="false"
+            v-for="(tag, index) in attractionList"
+            :key="index"
+            :attraction="tag"
+            remove="false"
     ></attraction-card>
   </div>
 </template>
 
 <script>
 import AttractionCard from "@/components/map/AttractionCard.vue";
+import http from "@/util/http-common";
 
 export default {
 
-  name: "SideBarAttractionList",
-  components:{
-    AttractionCard
-  },
-  data(){
-    return {
-      attractionList: [125266,125405],
-    }
-  },
-  methods: {
+    name: "SideBarAttractionList",
+    components: {
+        AttractionCard
+    },
 
-  }
+    data() {
+        return {
+            attractionList: [],
+            keyword: "광화문",
+            categoryCode:10,
+        }
+    },
+    created() {
+        http.get(`/attraction/select?keyword=${this.keyword}&categoryCode=${this.categoryCode}`).then(({ data }) => {
+            this.attractionList = data;
+        });
+    },
 }
 </script>
 
@@ -33,7 +39,6 @@ export default {
 .sidebar-attraction-list{
   height: calc(100% - 350px);
   margin:10px;
-  background-color: violet;
   overflow-y: scroll;
 }
 </style>
