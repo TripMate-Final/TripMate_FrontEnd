c1014f5995f434f3c0945f0e1649469d44cae771
yerimstar <yerimhuh@naver.com>
2023-05-24 09:19:58 +0900

    :rocket: feat: 일정 저장 버튼

--- src/components/plan/SideBarPlanList.vue
+++ src/components/plan/SideBarPlanList.vue
@@ -2,12 +2,15 @@
   <div>
     <b-sidebar id="sidebar-plan" width="400px" right shadow>
       <side-bar-plan-top></side-bar-plan-top>
-      <div v-for="dayItem in filteredList" :key="dayItem.day">
-        {{dayItem.day}}일차
-        <side-bar-plan-card v-for="(item,index) in dayItem.elements" :key="index" :attraction="item" :day=dayItem.day :index=index remove="false">
-        </side-bar-plan-card>
-      </div>
-    </b-sidebar>
+        <div class="plan-list">
+          <div v-for="dayItem in filteredList" :key="dayItem.day">
+            {{dayItem.day}}일차
+            <side-bar-plan-card v-for="(item,index) in dayItem.elements" :key="index" :attraction="item" :day=dayItem.day :index=index remove="false">
+            </side-bar-plan-card>
+          </div>
+        </div>
+        <button class="save-plan-button" v-if="selectedDay == 0" @click="savePlan">일정 저장</button>
+        </b-sidebar>
   </div>
 </template>
 <script>
@@ -38,10 +41,50 @@ export default {
         return this.planList.filter((dayItem) => dayItem.day === this.selectedDay);
       }
     }
-  }
+  },methods:{
+      savePlan(){
+
+      }
+    }
 }
 
 </script>
-<style scoped>
-
+<style scoped lang="scss">
+::v-deep{
+  #sidebar-plan{
+    .b-sidebar-body{
+      overflow-y: hidden;
+    }
+  }
+}
+.plan-list{
+  height: calc(100% - 260px);
+  overflow-y:scroll;
+}
+.save-plan-button{
+    right: 20px;
+    bottom: 20px;
+    position: fixed;
+    float: right;
+    height: 35px;
+    z-index: 3;
+    padding: 0px 10px;
+    background-color:#EFECEC;
+    border: 1px solid #0d172a;
+    border-radius: 1.5rem;
+    color: #0d172a;
+    font-family: "NanumSquareOTF","RocGrotesk-Wide";
+    font-size: 15px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: left;
+}
+.save-plan-button:hover{
+    background-color: #569A64;
+    color: #fff;
+    border: 0px;
+}
 </style>
